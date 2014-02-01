e1f2d412ff35cdb2255a8901b9a0b41efcf92f5f
Gix075 <gildo.giuliani@gmail.com>
2014-02-01 03:41:24 +0100

    Version 1.1.0 - First Upload
    First upload of donkeyGallery

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 donkeyGallery
 =============
 
-asynchronous ajax/php dynamic gallery
+*asynchronous ajax/php dynamic gallery*
+
+**donkeyGallery** is a tool for auto updating asyncronous gallery. It's work with php and javascript and allow you to load a gallery simply updating images inside a directory on your server and configuring it by javascript. When a gallery is configured you can update it simply adding new images inside the gallery dir. 
+
+How to install and use
+----------------------
+
+* Extract donkeyGallery zip pack on your local disk.
+* Update **"dnk-gallery"** dir on your server.
+* Update your image in specific dir inside *dnk-gallery/images/*
+* Include *dnk-gallery/css/donkeyGallery.min.css* in your html page.
+* Include also jQuery lib and *dnk-gallery/js/jquery.donkeyGallery.packed.min.js* in your html page.
+* Place a div in your html markup. Assign it an ID and leave it empty.
+* Initialize gallery (see below some javascript examples).
+
+Examples gallery configuration
+------------------------------
+**Default Gallery**
+```javascript
+$(document).on('ready', function(){
+  $("#your-gallery-id").donkeyGallery({
+      galleryPath : "dnk-gallery/images/your-gallery-dir/"  // Galley Path must end with a slash "/"
+  });
+});
+```
+**"Circle" style, "Gray" color and 100px x 100px thumbs**
+```javascript
+$(document).on('ready', function(){
+  $("#your-gallery-id").donkeyGallery({
+      galleryPath : "dnk-gallery/images/your-gallery-dir/"  // Galley Path must end with a slash "/"
+      style: "circle",
+      color: "gray",
+      thumbs: {
+          thumbW: 100,
+          thumbH: 100
+      }
+  });
+});
+```
+See more complete documentation and more examples of use at http://factory.brainleaf.eu/donkeyGallery
+
---
+++ dnk-gallery/php/galleryFunction.php
@@ -0,0 +1,113 @@
+<?php
+
+function imageList($dirname){
+	$arrayfiles=Array();
+	if(file_exists($dirname)){
+		$handle = opendir($dirname);
+		while (false !== ($file = readdir($handle))) { 
+			if(is_file($dirname.$file)){
+				array_push($arrayfiles,$file);
+			}
+		}
+		$handle = closedir($handle);
+	}
+	sort($arrayfiles);
+	return $arrayfiles;
+}
+
+
+function thumbGenerate($imageName,$galleryPath,$thumbSizes,$root) {
+	$imageName = $root.$galleryPath.$imageName;
+    $thumbDir = $root.$galleryPath.'thumb';
+    if (!file_exists($thumbDir)) {
+        mkdir($thumbDir, 0777, true);
+    }
+	$name = basename($imageName);
+	$image = imagecreatefromjpeg($imageName);
+	$filename = $root.$galleryPath.'thumb/thumb-'.$name;
+	
+	$thumb_width = $thumbSizes[0];
+	$thumb_height = $thumbSizes[0];
+	
+	$width = imagesx($image);
+	$height = imagesy($image);
+	
+	$original_aspect = $width / $height;
+	$thumb_aspect = $thumb_width / $thumb_height;
+	
+	if ( $original_aspect >= $thumb_aspect )
+	{
+	   // If image is wider than thumbnail (in aspect ratio sense)
+	   $new_height = $thumb_height;
+	   $new_width = $width / ($height / $thumb_height);
+	}
+	else
+	{
+	   // If the thumbnail is wider than the image
+	   $new_width = $thumb_width;
+	   $new_height = $height / ($width / $thumb_width);
+	}
+	
+	$thumb = imagecreatetruecolor( $thumb_width, $thumb_height );
+	
+	// Resize and crop
+	imagecopyresampled($thumb,
+					   $image,
+					   0 - ($new_width - $thumb_width) / 2, // Center the image horizontally
+					   0 - ($new_height - $thumb_height) / 2, // Center the image vertically
+					   0, 0,
+					   $new_width, $new_height,
+					   $width, $height);
+	imagejpeg($thumb, $filename, 100);
+    
+    md5gen($galleryPath,$root);
+
+}
+
+function md5gen($galleryPath,$root) {
+    $logDir = $root.$galleryPath.'/logs';
+    if (!file_exists($logDir)) {
+        mkdir($logDir, 0777, true);
+    }
+    $logFile = $logDir.'/gallery-log.txt';
+	$handle = fopen($logFile, 'w') or die('Cannot open file:  '.$logFile);
+    $md5 = md5_file($root.$galleryPath);
+	fwrite($handle, $md5);
+}
+
+function md5check($galleryPath,$root) {
+    $md5file = $root.$galleryPath.'/logs/gallery-log.txt';
+    $md5file = file_get_contents($md5file);
+    if (md5_file($root.$galleryPath) == $md5file) {
+        $result = "not-changed";
+    }else{
+        $result = "changed";
+    }
+    return $result;
+}
+
+function galleryGenerate($galleryPath,$forceGenerate,$thumbSizes,$elementId,$root) {
+    $md5check = md5check($galleryPath,$root);
+	$galleryImages = imageList($root.$galleryPath);
+	$galleryNumb = count($galleryImages);
+	for ($i=0; $i<$galleryNumb; $i++) {
+		if (($forceGenerate == 1) or (md5check == "changed")) {
+			thumbGenerate($galleryImages[$i],$galleryPath,$thumbSizes,$root);
+		}	
+        $alt = $i+1;
+		$code['jsonFancy'][] = "<a href=\"".$galleryPath.$galleryImages[$i]."\"><img src=\"".$galleryPath."thumb/thumb-".$galleryImages[$i]."\" alt=\"Image ".$alt."\" /></a>";
+        $code['jsonNoFancy'][] = "<img src=\"".$galleryPath."thumb/thumb-".$galleryImages[$i]."\" alt=\"Image ".$alt."\" />";
+		$galleryJsArray[] = "
+				{
+					'href'	: '".$galleryPath.$galleryImages[$i]."'
+				}
+			";
+	}
+	return $code;
+}
+
+
+	
+?>
+
+
